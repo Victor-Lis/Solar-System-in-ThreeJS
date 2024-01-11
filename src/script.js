8dbf03adb920a729bb6d1588f797aa2c637742e7
Victor-Lis <victorlisbronzo1@gmail.com>
2024-01-11 16:05:47 -0300

    v1
    - Upload das configurações iniciais do projeto.

---
+++ src/script.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+import starsTexture from './img/stars.jpg';
+import sunTexture from './img/sun.jpg';
+import mercuryTexture from './img/mercury.jpg';
+import venusTexture from './img/venus.jpg';
+import earthTexture from './img/earth.jpg';
+import marsTexture from './img/mars.jpg';
+import jupiterTexture from './img/jupiter.jpg';
+import saturnTexture from './img/saturn.jpg';
+import saturnRingTexture from './img/saturn ring.png';
+import uranusTexture from './img/uranus.jpg';
+import uranusRingTexture from './img/uranus ring.png';
+import neptuneTexture from './img/neptune.jpg';
+import plutoTexture from './img/pluto.jpg';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+renderer.render(scene, camera);
